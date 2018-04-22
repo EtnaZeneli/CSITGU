35869894f17bfbc8acd20ba0b6881f27853cbe09
EtnaZeneli <35538711+EtnaZeneli@users.noreply.github.com>
2018-04-22 17:04:15 -0400

    JavaScript Practice1

---
+++ info (1).js
@@ -0,0 +1,6 @@
+var name = "Etna Zeneli";
+var d = new Date()
+document.write("<span id ='footer'>Etna's Website</span><br />");
+document.write("<span id ='footer'>&#169 2018 Etna</span><br />");
+document.write("<span id ='footer'>Published on 4/22/2018</span><br />");
+document.write(Date())
